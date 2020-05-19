462260580ac906627ab9611a696c3e3e491bca3f
Luis Pedreira <luispedreiratome@gmail.com>
2020-05-19 17:04:23 -0500

    initial commit for section

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Split from "./composition/Split";
+function App() {
+  return (
+    <main className="App">
+      <Split />
+    </main>
+  );
+}
+export default App;
